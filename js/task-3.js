58b6df36dc689afec7467580afbd44d46d8cf32f
IvanBuzin <ua.buzin@gmail.com>
2023-11-05 11:01:18 +0200

    s

---
+++ js/task-3.js
@@ -0,0 +1,29 @@
+const profile = {
+    username: 'Jacob',
+    playTime: 300,
+
+    changeUsername(newName) {
+        for (const profil in profile) {
+            this.username = newName;
+        };
+    },
+    
+    updatePlayTime(hours) {
+        for (const profil in profile) {
+            this.playTime  = hours + 300;
+        };
+        
+    },
+    
+    getInfo() {
+        return `${this.username} has ${this.playTime}  active hours!`;
+    },
+}
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
